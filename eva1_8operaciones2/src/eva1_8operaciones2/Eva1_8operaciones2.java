a051e5febb49e6ff4587670ca213218008c0197e
MarcoRodriguez08 <L26550224@chihuahua2.TECNM.MX>
2026-09-14 18:59:49 -0600

    Add files via upload

---
+++ eva1_8operaciones2/src/eva1_8operaciones2/Eva1_8operaciones2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8operaciones2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_8operaciones2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int resu, val1 = 10, val2 = 5; //Se declaran las variables
+        //JAVA INICIALIZA EN AUTOMATICO LAS VARIABLES
+        //NUMERO 0, BOLEANO EN FALSO
+        resu = 100 +val1*(val1/val2);
+        System.out.print("Resu=");
+        System.out.println(resu);
+    }
+    
+}
