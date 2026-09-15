be43560ee622292dd84ee2b656a3336ba0ea4d93
MarcoRodriguez08 <L26550224@chihuahua2.TECNM.MX>
2026-09-14 19:00:09 -0600

    Add files via upload

---
+++ eva1_9operaciones_cadenas/src/eva1_9operaciones_cadenas/Eva1_9operaciones_cadenas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_9operaciones_cadenas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Ta,bien hay operadores por cadenas
+        //Concatenacion: pegar cadenas de testo
+        //cuando un operador hace diferentes acciones se le llama SOBRECARGA
+        String cade;
+        cade = "Hola" + "mundo";
+        System.out.println(cade);
+        cade ="valor ="+100;
+        System.out.println(cade);       
+        cade = "Valores boleanos ="+ false;
+        System.out.println(cade);
+    }
+    
+}
